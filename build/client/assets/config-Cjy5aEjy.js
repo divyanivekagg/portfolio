09d1ca8942b76f212d283823f22165a0400f6e00
“divyanivekagg” <divyaniveka319@gmail.com>
2024-07-27 15:02:07 +0530

    Adding build folder

---
+++ build/client/assets/config-Cjy5aEjy.js
@@ -0,0 +1,8 @@
+const i="Divyaniveka G G",n="ServiceNow Dev",o=["Js Enthusiast","☁️ Assosiate"],s="https://divyanivekagg.github.io/portfolio",t="divyanivekagg",e="divyaniveka-g-g-5aa7b2218/",a="https://github.com/divyanivekagg/portfolio",c=`██████─
+██══██─██─
+██║──██║
+██║──██║
+██████═└
+╚═════╝
+
+`,g={name:i,role:n,disciplines:o,url:s,github:t,linkedin:e,repo:a,ascii:c};export{g as c};
